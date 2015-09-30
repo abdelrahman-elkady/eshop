15e3397eddc78fbc1a2ed43a1d2e1a47565292fa
Abkreno <mohamedelhamamsy789@gmail.com>
2015-09-30 22:27:27 +0200

    completes the login form

--- templates/login.tpl.php
+++ templates/login.tpl.php
@@ -1,11 +1,23 @@
 <?php
-  $form_token = md5(uniqid('auth', true));
-  $_SESSION['form_token'] = $form_token;
+$form_token = md5(uniqid('auth', true));
+$_SESSION['form_token'] = $form_token;
 ?>
 
 <form action="index.php" method="post" role="form">
 
+	<legend>Login to your account</legend>
+	<div class="form-group">
+		<label for="email">Email</label>
+		<input type="email" class="form-control" id="email" placeholder="someone@somewhere.com">
+	</div>
+	<div class="form-group">
+		<label for="password">Password</label>
+		<input type="password" class="form-control" id="password" placeholder="•••••••">
+	</div>
 
+	<input type="hidden" name="form_token" value="<?php echo $form_token; ?>">
+
+	<button type="submit" class="btn btn-primary">Register</button>
 
 
 
