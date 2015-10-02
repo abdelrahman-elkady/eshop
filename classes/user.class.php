9fac2b1bdf3b3a9c426aafcdb981941e4956fa23
kady <elkadyabdelrahman@gmail.com>
2015-10-02 17:45:48 +0200

    Removing redirections on errors, Fixing default avatar insertion

--- classes/user.class.php
+++ classes/user.class.php
@@ -16,7 +16,7 @@ class User
      */
     public function registerUser()
     {
-        if (!(isset($_POST['first_name']) || isset($_POST['last_name']) || isset($_POST['email']) || isset($_POST['password']) || isset($_POST['avatar_file']) || isset($_POST['form_token']))) {
+        if (!(isset($_POST['first_name']) || isset($_POST['last_name']) || isset($_POST['email']) || isset($_POST['password']) || isset($_POST['form_token']))) {
             $this->errors[] = 'Submitting your request failed, please fill in all your information';
         } elseif ($_POST['form_token'] != $_SESSION['form_token']) {
             $this->errors[] = 'Something went wrong, please try again'; // ಠ ּ͜೦
@@ -25,33 +25,44 @@ class User
             $lastName = $_POST['last_name'];
             $email = $_POST['email'];
             $password = sha1($_POST['password']); // TODO: use more secure hashing mechanism
-            $avatar = $this->uploadAvatar();
+
+            if (!empty($_POST['avatar_file'])) {
+                $avatar = $this->uploadAvatar();
+            }
 
             try {
-                $stmt = $this->db->prepare('INSERT INTO `users`(first_name,last_name,email,password,avatar) VALUES (:firstName,:lastName,:email,:password,:avatar)');
+                // FIXME: Needs to be refactored !
+                if (!empty($_POST['avatar_file'])) {
+                    $stmt = $this->db->prepare('INSERT INTO `users`(first_name,last_name,email,password,avatar) VALUES (:firstName,:lastName,:email,:password,:avatar)');
+                    $stmt->bindParam(':avatar', $avatar, PDO::PARAM_STR);
+                }
+                else {
+                    $stmt = $this->db->prepare('INSERT INTO `users`(first_name,last_name,email,password) VALUES (:firstName,:lastName,:email,:password)');
+                }
 
                 $stmt->bindParam(':firstName', $firstName, PDO::PARAM_STR);
                 $stmt->bindParam(':lastName', $lastName, PDO::PARAM_STR);
                 $stmt->bindParam(':email', $email, PDO::PARAM_STR);
                 $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-                $stmt->bindParam(':avatar', $avatar, PDO::PARAM_STR);
+
 
                 $stmt->execute();
 
                 unset($_SESSION['form_token']);
-
             } catch (Exception $e) {
                 $this->errors[] = $e->getMessage();
             }
         }
     }
 
-    public function isSignedIn(){
-      return isset($_SESSION['user_id']);
+    public function isSignedIn()
+    {
+        return isset($_SESSION['user_id']);
     }
 
-    public function getErrors(){
-      return $this->errors;
+    public function getErrors()
+    {
+        return $this->errors;
     }
 
     private function uploadAvatar()
@@ -63,20 +74,17 @@ class User
             $check = getimagesize($_FILES['avatar_file']['tmp_name']);
             if ($check == false) {
                 $this->errors[] = 'File uploaded is not a proper image format';
-                Utils::redirect('index.php');
             }
         }
 
         // Check if file already exists
         if (file_exists($target_file)) {
             $this->errors[] = 'This file already exists !';
-            Utils::redirect('index.php');
         }
 
         if (!(move_uploaded_file($_FILES['avatar_file']['tmp_name'], $target_file))) {
             {
           $this->errors[] = 'Something went wrong !, please try again later';
-          Utils::redirect('index.php');
         }
         }
 
