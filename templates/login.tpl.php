3414c4aa370232d686ad55afe60d6708f4600ddb
Abkreno <mohamedelhamamsy789@gmail.com>
2015-10-03 11:15:19 +0200

    Adds name to inputs in login.tpl

--- templates/login.tpl.php
+++ templates/login.tpl.php
@@ -3,16 +3,16 @@ $form_token = md5(uniqid('auth', true));
 $_SESSION['form_token'] = $form_token;
 ?>
 
-<form action="index.php" method="post" role="form">
+<form action="login.php" method="post" role="form">
 
 	<legend>Login to your account</legend>
 	<div class="form-group">
 		<label for="email">Email</label>
-		<input type="email" class="form-control" id="email" placeholder="someone@somewhere.com">
+		<input type="email" name="email" class="form-control" id="email" placeholder="someone@somewhere.com">
 	</div>
 	<div class="form-group">
 		<label for="password">Password</label>
-		<input type="password" class="form-control" id="password" placeholder="•••••••">
+		<input type="password" name="password" class="form-control" id="password" placeholder="•••••••">
 	</div>
 
 	<input type="hidden" name="form_token" value="<?php echo $form_token; ?>">
@@ -20,5 +20,6 @@ $_SESSION['form_token'] = $form_token;
 	<button type="submit" class="btn btn-primary">Register</button>
 
 
-
 </form>
+
+<p>Not a member ? - <a href="register.php">Register now</a></p>
