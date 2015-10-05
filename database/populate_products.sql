f96884f3ede7b2692c125fe0937980d8bf5b6771
kady <elkadyabdelrahman@gmail.com>
2015-10-06 00:20:47 +0200

    Adding some seed script for products

---
+++ database/populate_products.sql
@@ -0,0 +1,17 @@
+INSERT INTO `products`(name,description,price,stock,picture) VALUES ('Table soccer','Table soccer set (aka. Babyfoot),
+for playing with your friends indoors',120.0,5,'assets/images/products/babyfoot.jpg');
+
+INSERT INTO `products`(name,description,price,stock,picture) VALUES ('Google Nexus 5X','LG Nexus 5X, a mobile phone!'
+  ,380.0,15,'assets/images/products/nexus-5x.jpg');
+
+INSERT INTO `products`(name,description,price,stock,picture) VALUES ('Google Nexus 6P','You can have calls with a bigger
+  Neuxs phone !',480.0,12,'assets/images/products/nexus-6p.jpg');
+
+INSERT INTO `products`(name,description,price,stock,picture) VALUES ('Crash Bandicoot 1','The best Video
+  game ever created',15.99,3,'assets/images/products/crash-1.jpg');
+
+INSERT INTO `products`(name,description,price,stock,picture) VALUES ('Windows 10','Powerful operating system
+  shipped with integrated spywares',399.99,100,'assets/images/products/windows.jpg');
+
+INSERT INTO `products`(name,description,price,stock,picture) VALUES ('Sugarless Gummybears','Sugarless Gummybears that would kill
+  you !!!',2.99,20,'assets/images/products/sugarless-gummy.jpg');
