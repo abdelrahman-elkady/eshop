067471eaa60ea16d303e9566ccf1488b6ce18a71
kady <elkadyabdelrahman@gmail.com>
2015-10-06 00:56:26 +0200

    Altering price type to hold only 2 digits after mark

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -14,7 +14,7 @@ CREATE TABLE `products`(
   `product_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `description` TEXT,
-  `price` DECIMAL(11,3) NOT NULL, -- our store is small, we will not rent islands here !
+  `price` DECIMAL(11,2) NOT NULL, -- our store is small, we will not rent islands here !
   `stock` INT UNSIGNED NOT NULL, -- TODO : represent out of stock by 0 or NULL ?
   `picture` VARCHAR(2083), -- TODO: DEFAULT Value ?
   PRIMARY KEY(`product_id`)
