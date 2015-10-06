cbd39c2bef5071283852756d82e6ded6e1faf9fa
kady <elkadyabdelrahman@gmail.com>
2015-10-06 23:46:11 +0200

    Redirecting guests to login on attempts to buy

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -7,11 +7,18 @@
         <p class="product-name"><?php echo $product['name']; ?></p>
         <h6 class="product-class"><?php echo $product['price']; ?> <i class="fa fa-usd currency"></i></h6>
       </div>
-      <?php if (intval($product['stock']) == 0): ?>
-        <button href="#" class="btn disabled btn-wide btn-block out-of-stock">OUT OF STOCK</button>
+
+      <?php if ($userObj->isSignedIn()): ?>
+        <?php if (intval($product['stock']) == 0): ?>
+          <button href="#" class="btn disabled btn-wide btn-block out-of-stock">OUT OF STOCK</button>
+        <?php else: ?>
+          <button href="#" class="btn btn-success btn-wide btn-block" data-toggle="modal" data-target="#add_to_cart_modal" data-id="<?php echo $product['product_id'];?>">BUY</button>
+        <?php endif; ?>
+
       <?php else: ?>
-        <button href="#" class="btn btn-success btn-wide btn-block" data-toggle="modal" data-target="#add_to_cart_modal" data-id="<?php echo $product['product_id'];?>">BUY</button>
+        <a href="login.php" class="btn btn-success btn-wide btn-block">BUY</a>
       <?php endif; ?>
+
     </div>
   </div>
 <?php endforeach; ?>
