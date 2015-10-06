8faddc4cb2c5e31cb99c879712cfe4db9c3f8fb7
kady <elkadyabdelrahman@gmail.com>
2015-10-06 16:54:07 +0200

    Fighting with some system paths and I won !

--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
 
+// Absolute path to current file's dir
+if (!defined('ABSPATH')) {
+    define('ABSPATH', realpath(dirname(__FILE__)).'/');
+}
+
 /* Database credentials
  *
  * It should define the following variables :
@@ -7,16 +12,11 @@
  * [ DB_HOSTNAME, DB_NAME, DB_USER, DB_PASS ]
  *
  */
-include_once 'config/db_credentials.php';
-
-// Absolute path to current file's dir
-if (!defined('ABSPATH')) {
-    define('ABSPATH', dirname(__FILE__).'/');
-}
+include_once ABSPATH.'db_credentials.php';
 
 // The host name
 if (!defined('HOSTNAME')) {
-  define('HOSTNAME','http://localhost:1234/');
+    define('HOSTNAME', 'http://localhost:1234/');
 }
 
 $db = new PDO('mysql:host='.DB_HOSTNAME.';dbname='.DB_NAME, DB_USER, DB_PASS);
@@ -25,9 +25,8 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 function __autoload($className)
 {
     // All our classes should be included into files with the same name but in lowercase!
-    include 'classes/'.strtolower($className).'.class.php';
+    include realpath(ABSPATH.'/../').'/classes/'.strtolower($className).'.class.php';
 }
 
 $userObj = new User($db);
 $productsObj = new Products($db);
-?>
